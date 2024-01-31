715fa64647d871ff28f6f08a8132975e3cca9932
BrendaLDelgado <brenda.l.delgado94@gmail.com>
2024-01-31 16:06:58 -0500

    creating random food for snake

--- script.js
+++ script.js
@@ -2,19 +2,23 @@
 const board = document.getElementById('game-board');
 
 // Define game variables
+const gridSize = 20;
 let snake = [{x:10, y:10}]; //where the snake will start in the game board
+let food = generateFood();
 
 // Draw game map, snake, food - resets the board
 function draw() {
     board.innerHTML = '';
     drawSnake();
+    drawFood();
 }
 
 // Draw snake
 function drawSnake() {
     snake.forEach((segment) => {
         const snakeElement = createGameElement('div', 'snake');
-        SVGTextPositioningElement(snakeElement, segment);
+        setPosition(snakeElement, segment);
+        board.appendChild(snakeElement);
     });
 }
 
@@ -28,4 +32,21 @@ function createGameElement(tag, className) {
 // Set position of the snake or food
 function setPosition(element, position) {
     element.style.gridColumn = position.x;
+    element.style.gridRow = position.y;
+}
+
+// Testing draw function
+draw();
+
+// Draw food function
+function drawFood() {
+    const foodElement = createGameElement('div', 'food');
+    setPosition(foodElement, food);
+    board.appendChild(foodElement);
+}
+
+function generateFood() {
+    const x = Math.floor(Math.random() * gridSize) + 1;
+    const y = Math.floor(Math.random() * gridSize) + 1;
+    return { x, y}
 }
